7fd22a55692eb40d61d1bef1c7c29f0551e3f763
Tom Storm <tstorm@invoca.com>
2018-01-09 01:33:58 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if board[index] between?(0,8)
+  if board[index] .between?(0,8)
     position_taken?
   else return false  
 end  
