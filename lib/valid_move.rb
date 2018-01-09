97797378be80e31c798a63d294acba83752d984d
Tom Storm <tstorm@invoca.com>
2018-01-09 01:40:50 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,6 +3,7 @@ def valid_move?(index)
   if index.between?(0,8)
     puts "asdf"
   else return false  
+  end
 end  
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
